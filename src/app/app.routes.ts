d6584e83c5a5ef1c51bd70ee5a84693ff4c141ac
habiba <habebahesham019@gmail.com>
2024-04-17 15:11:20 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { FirstComponent } from './components/first/first.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component: FirstComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'course', component: CoursesComponent},
+    {path: 'profile', component: ProfileComponent}
+];
---
+++ src/app/components/first/first.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-first',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './first.component.html',
+  styleUrl: './first.component.css'
+})
+export class FirstComponent {
+
+}
